818f417ebe663e2e8eb11cb7fcca945f71c2892e
Marcelo Albarracin <elchelamiente_25@hotmail.com>
2025-04-24 15:14:34 -0300

    Se ha implementado un nuevo controlador `UploadController` que permite la carga de imágenes, incluyendo la verificación de archivos, la creación de directorios y la generación de nombres de archivo únicos. La URL de la imagen cargada se devuelve como respuesta.

---
+++ api/CuentaClaraAPI/CuentaClaraAPI/Controllers/UploadController.cs
@@ -0,0 +1,54 @@
+﻿using CuentaClara.API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CuentaClara.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public UploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        [Route("image/users")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            // Crear carpeta si no existe
+            var uploadPath = Path.Combine(_environment.WebRootPath, "uploads/users");
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            // Generar nombre único
+            var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+            var filePath = Path.Combine(uploadPath, fileName);
+
+            // Guardar archivo
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // Devolver la URL para acceder a la imagen
+            var imageUrl = $"{Request.Scheme}://{Request.Host}/uploads/users/{fileName}";
+            return Ok(new
+            {
+                success = true,
+                imageUrl = imageUrl
+            });
+
+        }
+    }
+}
